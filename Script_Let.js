9d1bebd60f5d463d7d31a6bf10ab4dbcf3ce4dd1
roinacio <roinacio@santacasasaudesjc.com.br>
2021-09-01 00:17:13 -0300

    AULA DECLARANDO VARIAVEIS - ALURA

---
+++ Script_Let.js
@@ -0,0 +1,14 @@
+//let
+
+let forma = 'Retangulo'
+let altura = 5;
+let comprimento = 7;
+let area;
+
+if(forma === 'Retangulo'){
+    area = altura * comprimento;
+} else {
+    area = ((altura * comprimento) / 2);
+}
+
+console.log(area);
---
+++ Script_Var.js
@@ -0,0 +1,8 @@
+//var
+
+var altura = 5;
+var comprimento = 7;
+
+var area = altura * comprimento;
+
+console.log(area);
